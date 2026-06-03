999868344b5d2956c35ae12e67a720ac0699ed1e
Nimay Anand Desai <nimaydesai2005@gmail.com>
2026-06-03 10:31:41 +0530

    Update pps.c

--- LAB 4/pps.c
+++ LAB 4/pps.c
@@ -8,7 +8,7 @@ typedef struct {
 } Process;
 
 int main() {
-    printf("Name:Koushik\n" "USN:1WA24cs206\n" "PRG:Proportional share scheduling\n");
+    printf("Name:Nimay Anand Desai\n" "USN:1WA24CS189\n" "PRG:Proportional share scheduling\n");
     int n, total_tickets = 0;
     float total_T = 0.0;
 
