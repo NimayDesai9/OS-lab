9cccc2f77195f650e782f738bc2e35695686507a
Nimay Anand Desai <nimaydesai2005@gmail.com>
2026-06-03 10:31:15 +0530

    Update edf.c

--- LAB 4/edf.c
+++ LAB 4/edf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name:Koushik\n" "USN:1WA24cs206\n" "PRG:Earliest Deadline first (EDF) Algorithm\n");
+    printf("Name:Nimay Anand Desai\n" "USN:1WA24CS189\n" "PRG:Earliest Deadline first (EDF) Algorithm\n");
     int n, i;
 
     printf("Enter number of tasks: ");
